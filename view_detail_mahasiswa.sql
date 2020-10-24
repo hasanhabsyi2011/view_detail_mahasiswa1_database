16bc8890d509ae22451aa2bb56b19493c99ba5a5
hasanhabsyi2011 <71365610+hasanhabsyi2011@users.noreply.github.com>
2020-10-25 00:10:47 +0700

    Add files via upload

---
+++ view_detail_mahasiswa.sql
@@ -0,0 +1,10 @@
+CREATE VIEW `day26`.`detail_mahasiswa` 
+    AS
+(SELECT
+    `mahasiswa`.`nim`
+    , `mahasiswa`.`nama`
+    , `mahasiswa`.`jurusan`
+FROM
+    `day26`.`universitas`
+    INNER JOIN `day26`.`mahasiswa` 
+        ON (`universitas`.`jurusan` = `mahasiswa`.`jurusan`));
